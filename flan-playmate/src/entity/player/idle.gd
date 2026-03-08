6ef2947675d617bc97b3b22b95437fdd5dd84ff4
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2026-03-09 05:22:52 +0700

    feat: player movement

--- flan-playmate/src/entity/player/idle.gd
+++ flan-playmate/src/entity/player/idle.gd
@@ -1,4 +1,24 @@
 extends NodeState
 
-func _on_process(dt: float) -> void:
-    print(1)
+@export var sprite: AnimatedSprite2D
+@export var entity: CharacterBody2D
+
+@export_group("Movement Info")
+@export var friction: float = 0.85
+
+func _on_process(_dt: float) -> void:
+    GameInputs.update_movement()
+    
+    if GameInputs.is_confirm():
+        # TODO: Spawn bullets
+        pass
+    
+    entity.velocity *= friction
+    entity.move_and_slide()
+
+func _on_next_transition() -> void:
+    if GameInputs.is_move():
+        transition.emit("move")
+
+func _on_enter() -> void:
+    sprite.play("idle")
--- flan-playmate/src/entity/player/move.gd
+++ flan-playmate/src/entity/player/move.gd
@@ -1,4 +1,30 @@
 extends NodeState
 
-func _on_process(dt: float) -> void:
-    print(2)
+@export var sprite: AnimatedSprite2D
+@export var entity: CharacterBody2D
+
+@export_group("Movement Info")
+@export var accel: float = 50.
+@export var max_speed: float = 150
+@export var friction: float = 0.85
+
+func _on_process(_dt: float) -> void:
+    var dir = GameInputs.update_movement()
+    
+    if dir.x == 1.0:
+        sprite.play("right")
+    if dir.x == -1.0:
+        sprite.play("left")    
+    
+    if GameInputs.is_confirm():
+        # TODO: Spawn bullets
+        pass
+
+    entity.velocity += dir * accel
+    entity.velocity *= friction
+    entity.velocity = entity.velocity.clampf(-max_speed, max_speed)
+    entity.move_and_slide()
+
+func _on_next_transition() -> void:
+    if !GameInputs.is_move():
+        transition.emit("idle")
