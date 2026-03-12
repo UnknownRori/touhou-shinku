98f240108250811bea0520dcc9068bff2a282de9
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2026-03-13 04:27:01 +0700

    chore: move hp to top right

--- flan-playmate/src/ui/hp.gd
+++ flan-playmate/src/ui/hp.gd
@@ -2,6 +2,7 @@ extends HBoxContainer
 
 func _ready() -> void:
     GameState.player_hp.hp_change.connect(_on_update_hp)
+    $Label.text = str(GameState.player_hp.current_hp)
 
 func _on_update_hp(new_value: float):
     $Label.text = str(new_value)
